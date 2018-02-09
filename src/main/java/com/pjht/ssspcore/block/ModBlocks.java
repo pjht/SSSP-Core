5c192d9e19dde22016fdf07b3f0e818342befd4c
pjht <pjht@users.noreply.github.com>
2018-02-09 17:45:51 -0600

    Add block with custom model

--- src/main/java/com/pjht/ssspcore/block/ModBlocks.java
+++ src/main/java/com/pjht/ssspcore/block/ModBlocks.java
@@ -6,20 +6,24 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModBlocks {
 	public static BlockOre oreCopper = new BlockOre("ore_copper","oreCopper");
+	public static BlockPedestal pedestal = new BlockPedestal();
 	
 	public static void register(IForgeRegistry<Block> registry) {
 		registry.registerAll(
-				oreCopper
+				oreCopper,
+				pedestal
 		);
 	}
 	
 	public static void registerItemBlocks(IForgeRegistry<Item> registry) {
 		registry.registerAll(
-				oreCopper.createItemBlock()
+				oreCopper.createItemBlock(),
+				pedestal.createItemBlock()
 		);
 	}
 	
 	public static void registerItemModels() {
 		oreCopper.registerItemModel(Item.getItemFromBlock(oreCopper));
+		pedestal.registerItemModel(Item.getItemFromBlock(pedestal));
 	}
 }
