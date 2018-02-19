2edd4ecce938ca43e308869352ddfc9931ea8730
pjht <pjht@users.noreply.github.com>
2018-02-18 18:06:59 -0600

    Add silicon item

--- src/main/java/com/pjht/ssspcore/item/ModItems.java
+++ src/main/java/com/pjht/ssspcore/item/ModItems.java
@@ -6,14 +6,16 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 	public static ItemOre ingotCopper = new ItemOre("ingot_copper", "ingotCopper");
-	
+	public static ItemOre silicon = new ItemOre("silicon","itemSilicon");
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.registerAll(
-				ingotCopper
+				ingotCopper,
+				silicon
 		);
 	}
 	
 	public static void registerModels() {
 		ingotCopper.registerItemModel();
+		silicon.registerItemModel();
 	}
 }
