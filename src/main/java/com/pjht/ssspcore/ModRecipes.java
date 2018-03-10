4e9fcdc570fd873e9f3ff107b5f316d731491a0d
pjht <pjht@users.noreply.github.com>
2018-03-10 11:00:13 -0600

    Remove sand to silicon smelting recipe

--- src/main/java/com/pjht/ssspcore/ModRecipes.java
+++ src/main/java/com/pjht/ssspcore/ModRecipes.java
@@ -3,13 +3,11 @@ package com.pjht.ssspcore;
 import com.pjht.ssspcore.block.ModBlocks;
 import com.pjht.ssspcore.item.ModItems;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 	public static void init() {
 		GameRegistry.addSmelting(ModBlocks.oreCopper, new ItemStack(ModItems.ingotCopper), 0.7f);
-		GameRegistry.addSmelting(Blocks.SAND, new ItemStack(ModItems.silicon), 0.7f);
 	}
 }
