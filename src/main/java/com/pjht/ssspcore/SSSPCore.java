a01f616a9e9f3606aa32403d6057125f3e2383e1
Donovan Lampa <donovan.lampa@gmail.com>
2018-03-10 11:30:31 -0600

    Add coke fuel

--- src/main/java/com/pjht/ssspcore/SSSPCore.java
+++ src/main/java/com/pjht/ssspcore/SSSPCore.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = SSSPCore.modId, name = SSSPCore.name, version = SSSPCore.version)
 public class SSSPCore {
--- src/main/java/com/pjht/ssspcore/item/ModItems.java
+++ src/main/java/com/pjht/ssspcore/item/ModItems.java
@@ -7,15 +7,18 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModItems {
 	public static ItemOre ingotCopper = new ItemOre("ingot_copper", "ingotCopper");
 	public static ItemOre silicon = new ItemOre("silicon","itemSilicon");
+	public static ItemFuel coke = new ItemFuel("coke","itemCoke", 1000);
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.registerAll(
 				ingotCopper,
-				silicon
+				silicon,
+				coke
 		);
 	}
 	
 	public static void registerModels() {
 		ingotCopper.registerItemModel();
 		silicon.registerItemModel();
+		coke.registerItemModel();
 	}
 }
